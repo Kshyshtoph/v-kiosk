a89ecb7e7985603935415a66555b0315630ec8d9
Krzysztof Baum <baumkrzysztof@gmail.com>
2023-12-04 15:09:48 +0100

    prod and dev base urls

--- html/index.js
+++ html/index.js
@@ -1,5 +1,13 @@
-const backendUrl = "http://127.0.0.1:3000"
 const body = document.querySelector('body')
+
+const baseCb = () => {
+    try{
+        return backendBaseUrl
+    } catch(e){
+        return "api"
+    }
+}
+const backendUrl = baseCb()
 
 const createCard = (data) => {
     const card = document.createElement('div');
@@ -8,10 +16,12 @@ const createCard = (data) => {
     img.src = data.thumbnail_url;
     card.appendChild(img)
     const p = document.createElement('p')
-     p.textContent = data.short_desc
+    p.textContent = data.short_desc
     card.appendChild(p)
     body.appendChild(card)
 }
 
+const d00pa = "ENV_DOOPA"
 
-fetch(backendUrl + "/titles").then(res=>res.json()).then(r=>{console.log(r); return r }).then(data=> data.forEach(entry=> createCard(entry)))
+
+fetch(backendUrl + "/titles").then(res => res.json()).then(r => { console.log(r); return r }).then(data => data.forEach(entry => createCard(entry)))
